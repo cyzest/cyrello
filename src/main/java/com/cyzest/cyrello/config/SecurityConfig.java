c471dc1cfebbabb5dcb2562276a1c546b2b1c026
cyzest <cyzest@nate.com>
2018-09-12 13:06:31 +0900

    스프링 시큐리티 설정 리팩토링

--- src/main/java/com/cyzest/cyrello/config/SecurityConfig.java
+++ src/main/java/com/cyzest/cyrello/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -24,19 +23,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/h2", "/h2/**");
-    }
-
-    @Override
     protected void configure(HttpSecurity http) throws Exception {
 
         ObjectMapper objectMapper = Jackson2ObjectMapperBuilder.json().build();
 
         http.csrf().disable()
+                .antMatcher("/api/**")
                 .authorizeRequests()
                 .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-                .antMatchers(HttpMethod.GET, "/").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/users").permitAll()
                 .anyRequest().authenticated()
                 .and()
@@ -61,7 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", new CorsConfiguration());
+        source.registerCorsConfiguration("/api/**", new CorsConfiguration());
         return source;
     }
 
